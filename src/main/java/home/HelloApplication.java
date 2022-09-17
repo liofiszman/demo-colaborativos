78c43f31480d535a73d17175efd4bab44278b67a
Lionel Fiszman <lfiszman@plewy.com>
2022-09-17 18:19:28 -0300

    Initial commit

---
+++ src/main/java/home/HelloApplication.java
@@ -0,0 +1,201 @@
+package home;
+
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        home(stage);
+    }
+
+    public static void home(Stage stage) throws IOException {
+        showStage(stage, "hello-view.fxml");
+    }
+    public static void asistencia(Stage stage) throws IOException {
+        showStage(stage, "asistencia-view.fxml");
+    }
+    public static void actividades(Stage stage) throws IOException {
+        showStage(stage, "actividades-view.fxml");
+    }
+    public static void calendario(Stage stage) throws IOException{
+        showStage(stage, "calendario-view.fxml");
+    }
+    public static void conformidad(Stage stage) throws IOException {
+        showStage(stage, "conformidad-view.fxml");
+    }
+
+    public static void showStage(Stage stage, String viewName) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource(viewName));
+        Scene scene = new Scene(fxmlLoader.load(), 640, 480);
+        stage.setTitle("Sistema de turnos para taller mecánico");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void diario(Stage stage) throws IOException {
+        TableView<Arreglo> table = new TableView<Arreglo>();
+        ObservableList<Arreglo> arreglos = getArreglos();
+        table.setItems(arreglos);
+
+        //Creating columns
+        TableColumn clienteCol = new TableColumn("Patente");
+        clienteCol.setCellValueFactory(new PropertyValueFactory<>("cliente"));
+        TableColumn mecanicoCol = new TableColumn("Mecanico");
+        mecanicoCol.setCellValueFactory(new PropertyValueFactory("mecanico"));
+        TableColumn especialidadCol = new TableColumn("Especialidad");
+        especialidadCol.setCellValueFactory(new PropertyValueFactory("especialidad"));
+        TableColumn conformidadCol = new TableColumn("Conformidad");
+        conformidadCol.setCellValueFactory(new PropertyValueFactory("conformidad"));
+        mecanicoCol.setCellValueFactory(new PropertyValueFactory("mecanico"));
+        TableColumn servicioCol = new TableColumn("Servicio");
+        servicioCol.setCellValueFactory(new PropertyValueFactory("servicio"));
+        table.getColumns().addAll(clienteCol, mecanicoCol, especialidadCol, conformidadCol, servicioCol);
+
+        VBox vbox = new VBox(table);
+        Scene scene = new Scene(vbox, 800, 600);
+        stage.setScene(scene);
+        stage.show();
+
+        //showStage(stage, "diario-view.fxml");
+    }
+
+    public static void messelector(Stage stage) throws IOException {
+        showStage(stage, "messelector-view.fxml");
+    }
+
+    public static void mensual(Stage stage) throws IOException {
+        TableView<Arreglo> table = new TableView<Arreglo>();
+        ObservableList<Arreglo> arreglos = getArreglos();
+        table.setItems(arreglos);
+
+        //Creating columns
+        TableColumn companiaCol = new TableColumn("Aseguradora");
+        companiaCol.setCellValueFactory(new PropertyValueFactory<>("compania"));
+        TableColumn fechaCol = new TableColumn("Fecha");
+        fechaCol.setCellValueFactory(new PropertyValueFactory<>("fecha"));
+        TableColumn clienteCol = new TableColumn("Cliente");
+        clienteCol.setCellValueFactory(new PropertyValueFactory<>("cliente"));
+        TableColumn mecanicoCol = new TableColumn("Mecanico");
+        mecanicoCol.setCellValueFactory(new PropertyValueFactory("mecanico"));
+        TableColumn servicioCol = new TableColumn("Servicio");
+        servicioCol.setCellValueFactory(new PropertyValueFactory("servicio"));
+        TableColumn conformidadCol = new TableColumn("Conformidad");
+        conformidadCol.setCellValueFactory(new PropertyValueFactory("conformidad"));
+        table.getColumns().addAll(companiaCol, fechaCol, clienteCol, mecanicoCol, servicioCol, conformidadCol);
+
+        VBox vbox = new VBox(table);
+        Scene scene = new Scene(vbox, 800, 600);
+        stage.setScene(scene);
+        stage.show();
+
+        //showStage(stage, "mensual-view.fxml");
+    }
+
+    private static ObservableList<String> getEspecialidades() {
+        ObservableList<String> list = FXCollections.observableArrayList(
+                "frenos",
+                "electricidad",
+                "tren delantero y amortiguación",
+                "encendido y carburación",
+                "chapa y pintura",
+                "mecánica en general"
+        );
+
+        return list;
+    }
+
+    private static ObservableList<Arreglo> getArreglos() {
+        ObservableList<Arreglo> list = FXCollections.observableArrayList(
+                new Arreglo("03/09/2022","USP784", "SSJ-32", "mecánica en general",
+                        "Conforme", "Cambio de aceite", "Sancor"),
+                new Arreglo("06/09/2022","ASD123", "SSJ-32", "frenos",
+                        "NO Conforme", "Alineado y balanceado", "Sancor"),
+                new Arreglo("06/09/2022","AR787UX", "SSJ-32","mecánica en general",
+                        "Conforme", "Cambio de bateria", "Sancor")
+        );
+
+        return list;
+    }
+
+        public static class Arreglo {
+
+            public Arreglo(String fecha, String cliente, String mecanico, String especialidad,
+                           String conformidad, String servicio, String compania) {
+                setCliente(cliente);
+                setMecanico(mecanico);
+                setConformidad(conformidad);
+                setFecha(fecha);
+                setServicio(servicio);
+                setCompania(compania);
+                setEspecialidad(especialidad);
+            }
+
+            private String Especialidad;
+
+            public void setEspecialidad(String value) { Especialidad = value; }
+            public String getEspecialidad() { return Especialidad; }
+
+            private String Compania;
+
+            public void setCompania(String value) { Compania = value; }
+            public String getCompania() { return Compania; }
+
+            private String Servicio;
+
+            public void setServicio(String value) { Servicio = value; }
+            public String getServicio() { return Servicio; }
+
+            private String Fecha;
+
+            public void setFecha(String value) { Fecha = value; }
+            public String getFecha() { return Fecha; }
+
+            private String Cliente;
+
+            public void setCliente(String value) { Cliente = value; }
+            public String getCliente() { return Cliente; }
+
+            private String Mecanico;
+            public void setMecanico(String value) { Mecanico = value; }
+            public String getMecanico() { return Mecanico; }
+
+            private String Conformidad;
+            public void setConformidad(String value) { Conformidad = value; }
+            public String getConformidad() { return Conformidad; }
+        }
+
+        private String Fecha;
+
+        public void setFecha(String value) { Fecha = value; }
+        public String getFecha() { return Fecha; }
+
+        private String Cliente;
+
+        public void setCliente(String value) { Cliente = value; }
+        public String getCliente() { return Cliente; }
+
+        private String Mecanico;
+        public void setMecanico(String value) { Mecanico = value; }
+        public String getMecanico() { return Mecanico; }
+
+        private String Conformidad;
+        public void setConformidad(String value) { Conformidad = value; }
+        public String getConformidad() { return Conformidad; }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ src/main/java/home/HelloController.java
@@ -0,0 +1,102 @@
+package home;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    private AnchorPane showPane;
+
+    @FXML
+    protected void onRegistrarAsistenciaButtonClick(ActionEvent event) throws IOException {
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        HelloApplication.asistencia(stage);
+    }
+    @FXML
+    protected void onRegistrarActividadesButtonClick(ActionEvent event) throws IOException {
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        HelloApplication.actividades(stage);
+    }
+    @FXML
+    protected void onRegistrarCalendarioButtonClick(ActionEvent event) throws IOException {
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        HelloApplication.calendario(stage);
+    }
+    @FXML
+    protected void onRegistrarConformidadButtonClick(ActionEvent event) throws IOException {
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        HelloApplication.conformidad(stage);
+    }
+    @FXML
+    protected void onReporteDiarioButtonClick(ActionEvent event) throws IOException {
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        HelloApplication.diario(stage);
+    }
+    @FXML
+    protected void onReporteMensualButtonClick(ActionEvent event) throws IOException {
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        HelloApplication.messelector(stage);
+    }
+
+    @FXML
+    protected void onObtenerReporteMensualButtonClick(ActionEvent event) throws IOException {
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        HelloApplication.mensual(stage);
+    }
+
+    @FXML
+    protected void backToHome(ActionEvent event) throws IOException {
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        HelloApplication.home(stage);
+    }
+
+    // ASISTENCIA
+    @FXML
+    private Label datosTurnoText;
+    @FXML
+    private Label datosTurnoSecondText;
+    @FXML
+    private Label actividadesText;
+    @FXML
+    private Label insumosText;
+
+    @FXML
+    protected void datosTurnoButtonClick() {
+        datosTurnoText.setText("Turno para el 10/10 a las 13:30 hs");
+        datosTurnoSecondText.setText("Mecánico SSJ-32, especialidad General");
+
+        actividadesText.setText("Alineado y balanceado.");
+        insumosText.setText("Sin repuestos utilizados.");
+    }
+
+    //CALENDARIO
+    @FXML
+    private Label patenteText;
+    @FXML
+    private Label clienteText;
+    @FXML
+    private Label especialidadText;
+    @FXML
+    private Label fechaTurnoText;
+
+    @FXML
+    private DatePicker datePicker;
+
+    public void confirmarTurnoButtonClick(ActionEvent actionEvent) {
+        patenteText.setText("OSJ-321");
+        clienteText.setText("Juan Carlos Castro");
+        especialidadText.setText("Frenos");
+        fechaTurnoText.setText("Turno para el 10/10 a las 13:30 hs");
+    }
+}
