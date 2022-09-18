07977fad8dc6e54b3e68d7dde03c84cebf6804cc
Yago Marti <y.g.marti@distillery.com>
2022-09-17 23:41:29 -0300

    sarasa

--- src/main/java/Controllers/AsistenciaController.java
+++ src/main/java/Controllers/AsistenciaController.java
@@ -9,6 +9,7 @@ import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 import java.net.URL;
+import java.time.format.DateTimeFormatter;
 import java.util.ResourceBundle;
 
 public class AsistenciaController extends BaseController  {
@@ -19,9 +20,11 @@ public class AsistenciaController extends BaseController  {
     protected void datosTurnoButtonClick(ActionEvent event) {
         Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
 
-        Turno turno = new Turno();
+        Opcion turno = new Opcion();
+        String formatoCalendario = turno.getFecha().toString();
+        String FormatoHora = turno.getFecha().format(DateTimeFormatter.ofPattern("HH:mm"));
         // TODO : get turno from number.
-        datosTurnoText.setText("Turno para el 10/10 a las 13:30 hs");
+        datosTurnoText.setText("Turno para el "+formatoCalendario+" a las "+FormatoHora+" hs");
         datosTurnoSecondText.setText("Mecánico SSJ-32, especialidad General");
     }
 
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,11 @@
 module com.example.demo {
-requires javafx.controls;
-requires javafx.fxml;
-requires org.controlsfx.controls;
-requires com.dlsc.formsfx;
-requires org.kordamp.bootstrapfx.core;
-opens home to javafx.fxml;
-exports home;
-opens Controllers to javafx.fxml;
-exports Controllers;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+    opens home to javafx.fxml;
+    exports home;
+    opens Controllers to javafx.fxml;
+    exports Controllers;
 }
